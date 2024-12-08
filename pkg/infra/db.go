1d8be061088884e2230bf22b051537f45221a1b7
da1ch1a <sss.fbs7.fre.e@gmail.com>
2024-12-08 16:41:21 +0900

    use env

--- pkg/infra/db.go
+++ pkg/infra/db.go
@@ -21,9 +21,8 @@ func InitDb() *sql.DB {
 		User:                 os.Getenv("DB_USER"),
 		Passwd:               os.Getenv("DB_PASSWORD"),
 		Net:                  "tcp",
-		// Addr:                 "127.0.0.1:33060",
-		Addr:                 "go-todo-mysql:3306",
-		DBName:               "go_todo_local",
+		Addr:                 os.Getenv("DB_HOST"),
+		DBName:               os.Getenv("DB_NAME"),
 		AllowNativePasswords: true, // rootでログインするため
 		Params: map[string]string{
 			// MySQLのDATEとDATETIMEはGoで出力するとデフォルトは[]byteになるので、これをtime.Timeに変換するためにparseTime=trueを指定する
